ae1538a9dfe92a2641a5a84e91ea097ae8e809a5
王珍 <865820954@qq.com>
2018-11-20 22:02:22 +0800

    knn

--- app/batches/run_knn_model.py
+++ app/batches/run_knn_model.py
@@ -18,7 +18,6 @@ def execute(start_date='', end_date=''):
                       sample_interval=sample_interval,
                       pre_predict_interval=pre_predict_interval,
                       memory_size=memory_size,
-                      single_code_id=3
             )
             model.run()
 
