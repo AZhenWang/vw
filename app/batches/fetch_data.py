7051b57ef3eef44617246197bf3e4147b26f0412
王珍 <865820954@qq.com>
2018-12-08 16:31:14 +0800

    fetch_data

--- app/batches/fetch_data.py
+++ app/batches/fetch_data.py
@@ -9,6 +9,7 @@ def execute(start_date='', end_date=''):
     worker.set_trade_dates()
     worker.set_code_list()
     if not worker.trade_dates.empty:
-        ts_apis = ['daily', 'daily_basic', 'adj_factor']
+        # ts_apis = ['daily', 'daily_basic', 'adj_factor']
+        ts_apis = ['daily']
         for api in ts_apis:
             worker.query(api)
