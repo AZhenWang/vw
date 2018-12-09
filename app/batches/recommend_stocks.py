a8a5c509c154d25334e6661e44bbce7c45b49559
王珍 <865820954@qq.com>
2018-12-09 22:32:38 +0800

    recommend

--- app/batches/recommend_stocks.py
+++ app/batches/recommend_stocks.py
@@ -39,42 +39,42 @@ def execute(start_date='', end_date=''):
     #         data.to_sql('recommend_stocks', DB.engine, index=False, if_exists='append', chunksize=1000)
 
     trade_cal = DB.get_open_cal_date(end_date=end_date, period=2)
+    code_list = DB.get_code_list(list_status='L')
     for i in range(1, len(trade_cal)):
         current_date_id = trade_cal.iloc[i]['date_id']
         last_date_id = trade_cal.iloc[i - 1]['date_id']
 
-    code_list = DB.get_code_list(list_status='L')
-    data = pd.DataFrame(columns=fields_map['recommend_stocks'])
-    j = 0
+        data = pd.DataFrame(columns=fields_map['recommend_stocks'])
+        j = 0
 
-    for code_id in code_list['code_id']:
-        #获取前五个回报值最高的group
-        top_five_group = cr.get_info(code_id=code_id,date_id=current_date_id,limit=5)
-        groups = []
-        info = {}
-        for val in top_five_group:
-            groups.append(val.feature_group_number)
-            info[val.feature_group_number] = val
-        #获取这五个昨天的回报值
-        last_top_five_group = cr.get_info(code_id=code_id, date_id=last_date_id, limit=5,feature_group_number=groups)
+        for code_id in code_list['code_id']:
+            # 获取前五个回报值最高的group
+            top_five_group = cr.get_info(code_id=code_id,date_id=current_date_id, limit=5)
+            groups = []
+            info = {}
+            for val in top_five_group:
+                groups.append(val.feature_group_number)
+                info[val.feature_group_number] = val
+            #获取这五个昨天的回报值
+            last_top_five_group = cr.get_info(code_id=code_id, date_id=last_date_id, limit=5, feature_group_number=groups)
 
-        star = []
-        for val in last_top_five_group:
-            yester = val.classifier_v
-            today = info[val.feature_group_number].classifier_v
-            if yester > 0.01 and yester < today:
-                star.append(yester)
-        star_idx = len(star)
-        if star_idx > 0:
-            average = np.array(star).mean()
-            data.loc[j] = {
-                                'code_id': code_id,
-                                'date_id': current_date_id,
-                                'recommend_type': 'classified_v',
-                                'star_idx': star_idx,
-                                'average': average,
-                            }
-            data.to_sql('recommend_stocks', DB.engine, index=False, if_exists='append', chunksize=1000)
-        j += 1
+            star = []
+            for val in last_top_five_group:
+                yester = val.classifier_v
+                today = info[val.feature_group_number].classifier_v
+                if yester > 0.01 and yester < today:
+                    star.append(yester)
+            star_idx = len(star)
+            if star_idx > 0:
+                average = np.array(star).mean()
+                data.loc[j] = {
+                                    'code_id': code_id,
+                                    'date_id': current_date_id,
+                                    'recommend_type': 'classified_v',
+                                    'star_idx': star_idx,
+                                    'average': average,
+                                }
+                data.to_sql('recommend_stocks', DB.engine, index=False, if_exists='append', chunksize=1000)
+            j += 1
 
 
