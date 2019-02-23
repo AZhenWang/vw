ce5d068f9911bb6381ae9602f910850b9dc4a806
王珍 <865820954@qq.com>
2019-02-23 19:40:19 +0800

    recommend

--- app/batches/recommend_stocks.py
+++ app/batches/recommend_stocks.py
@@ -1,5 +1,6 @@
 from app.saver.logic import DB
 import pandas as pd
+import numpy as np
 from app.models.pca import Pca
 from app.common.function import send_email
 from app.common.function import knn_predict
@@ -13,7 +14,6 @@ n_components = 2
 def execute(start_date='', end_date=''):
     trade_cal = DB.get_open_cal_date(end_date=end_date, period=2)
     current_date_id = trade_cal.iloc[-1]['date_id']
-    yester_date_id = trade_cal.iloc[0]['date_id']
     cal_date = trade_cal.iloc[-1]['cal_date']
     logs = DB.get_recommended_stocks(cal_date=cal_date, recommend_type='pca')
     logs = logs[logs['star_idx'] > 1]
@@ -35,6 +35,14 @@ def execute(start_date='', end_date=''):
             if not lastestrecommend_logs.empty:
                 last_recommend_date = lastestrecommend_logs.iloc[-1]['cal_date']
                 last_recommend_star = lastestrecommend_logs.iloc[-1]['star_idx']
+
+            sample_pca, sample_prices, sample_Y = pca.run(code_id=code_id, sample_len=0,
+                                                          n_components=n_components,
+                                                          pre_predict_interval=pre_predict_interval,
+                                                          return_y=True)
+            pct_std = np.std(sample_Y)
+            pct_mean = np.mean(sample_Y)
+
             content = {
                 'ts_code': logs.iloc[i]['ts_code'],
                 'star_idx': logs.iloc[i]['star_idx'],
@@ -43,14 +51,11 @@ def execute(start_date='', end_date=''):
                 'average': logs.iloc[i]['average'],
                 'moods': logs.iloc[i]['moods'],
                 'amplitude': logs.iloc[i]['amplitude'],
+                'pct_mean': round(pct_mean, 1),
+                'pct_std': round(pct_std, 1),
                 'knn_v': '',
             }
 
-            sample_pca, sample_prices, sample_Y = pca.run(code_id=code_id, sample_len=0,
-                                                          n_components=n_components,
-                                                          pre_predict_interval=pre_predict_interval,
-                                                          return_y=True)
-            pca = Pca(cal_date=cal_date)
             knn_v = ''
             for predict_idx in sample_Y.index[-5:]:
                 y_hat = knn_predict(sample_pca, sample_Y, k=1, sample_interval=122,
@@ -60,6 +65,7 @@ def execute(start_date='', end_date=''):
 
             recommend_stocks.loc[i] = content
     if not recommend_stocks.empty:
+        recommend_stocks.sort_values(by=['start_idx', 'moods'], ascending=[False, False])
         recommend_text = recommend_stocks.to_string(index=False)
 
         msgs.append(MIMEText(recommend_text, 'plain', 'utf-8'))
