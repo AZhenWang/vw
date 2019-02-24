4976f2d4c6d004f9822bce5d82af59ff443fe0a1
王珍 <865820954@qq.com>
2019-02-24 22:07:40 +0800

    focus

--- app/batches/focus.py
+++ app/batches/focus.py
@@ -68,7 +68,7 @@ def execute(start_date='', end_date=''):
                     'star_idx': holdings[-1],
                     'average': round(mean, 2),
                     'amplitude': round(y_hat, 1),
-                    'moods': round(y1_y1, 1)
+                    'moods': round(y1_y1, 2)
                 }
             i += 1
         if not new_rows.empty:
