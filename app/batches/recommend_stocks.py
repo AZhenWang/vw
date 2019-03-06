52ce30200849d6b30dab718beb3fbf9631278f92
王珍 <865820954@qq.com>
2019-03-06 23:12:25 +0800

    f

--- app/batches/recommend_stocks.py
+++ app/batches/recommend_stocks.py
@@ -98,8 +98,9 @@ def execute(start_date='', end_date=''):
                                               closed_date_id=closed_date_id)
                     break
 
-                elif ((later_daily['close'] - later_daily['open']) / later_daily['open']) > 0.01\
-                        and later_daily['close'] > (np.max([recommended_daily.at[0, 'close'], second_daily.at[0, 'close'], second_daily.at[1, 'close']]))*1.015:
+                elif ((later_daily['close'] - later_daily['open']) / later_daily['open']) > 0.01 \
+                        and later_daily['pct_chg'] > 2 \
+                        and later_daily['close'] > (np.max([recommended_daily.at[0, 'high'], second_daily.at[0, 'high'], second_daily.at[1, 'high']]))*1.01:
                     holding_date_id = date_id
                     holding_at = later_daily['cal_date']
                     DB.update_focus_stock_log(code_id=code_id, recommended_date_id=recommended_date_id,
