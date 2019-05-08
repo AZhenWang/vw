6a5f070baf4f8582a3668961792bb6b3bfdb4d66
王珍 <865820954@qq.com>
2019-05-08 21:30:46 +0800

    s

--- app/batches/focus_tp.py
+++ app/batches/focus_tp.py
@@ -27,9 +27,9 @@ def execute(start_date='', end_date=''):
     full_cal = DB.get_cal_date(start_date=first_date, end_date=end_date)
 
     cal_length = len(trade_cal)
-    # codes = DB.get_code_list_before_date(min_list_date=first_date,)
-    # code_ids = codes['code_id']
-    code_ids = [19]
+    codes = DB.get_code_list_before_date(min_list_date=first_date,)
+    code_ids = codes['code_id']
+    # code_ids = [2772]
     # code_ids = [1475,  2756]
     # 238: 东方电子，462：豫能控股， 2756：红阳能源， 2274：莲花健康， 2308：天津松江
     for code_id in code_ids:
