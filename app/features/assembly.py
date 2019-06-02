5ba90f8729eb02c12db607e653d88bea1134c59b
王珍 <865820954@qq.com>
2019-06-02 21:32:29 +0800

    s

--- app/features/assembly.py
+++ app/features/assembly.py
@@ -160,11 +160,12 @@ class Assembly(object):
         dr_nagetive_SMA10 = dr_nagetive.rolling(window=10).mean()
         RSI5 = dr_position_SMA5 / (dr_position_SMA5 - dr_nagetive_SMA5)
         RSI10 = dr_position_SMA10 / (dr_position_SMA10 - dr_nagetive_SMA10)
-        # data['high'] = data['high'] + 0.001
-        # Amplitude = (data['close'] - data['open']) / (data['high'] - data['low'])
-        # Amplitude = Amplitude[data['pct_chg'] < 0].fillna(-1)
-        # Amplitude = Amplitude[data['pct_chg'] >= 0].fillna(1)
-        # Amplitude.fillna(0)
+        data['high'] = data['high']
+        Amplitude = (data['close'] - data['open']) / (data['high'] - data['low'])
+        Amplitude[data['pct_chg'] < 0].fillna(-1)
+        Amplitude[data['pct_chg'] >= 0].fillna(1)
+        Amplitude.fillna(0)
+
         feature_dict = {}
         for feature in self.features['name']:
             feature_dict[feature] = eval(feature)
