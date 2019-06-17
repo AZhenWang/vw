817182f211d40ef46ea88dc01f184ea29bdfbf92
王珍 <865820954@qq.com>
2019-06-17 20:26:19 +0800

    s

--- app/batches/statistic_up_stock.py
+++ app/batches/statistic_up_stock.py
@@ -4,6 +4,7 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
 from app.common.function import get_cum_return_rate, get_buy_sell_points
 
 ratio_lable = {
@@ -20,31 +21,32 @@ ratio_lable = {
 
 
 def execute(start_date='', end_date=''):
-    trade_cal = DB.get_open_cal_date(end_date=end_date, period=60)
+    trade_cal = DB.get_open_cal_date(end_date=end_date, period=21*6)
     start_date_id = trade_cal.iloc[0]['date_id']
     end_date_id = trade_cal.iloc[-1]['date_id']
-    data = DB.count_threshold_group_by_date_id(start_date_id=start_date_id, end_date_id=end_date_id)
+    index_code = '000905.SH'
+    # index_code = '000001.SH'
+    statistic_data = DB.count_threshold_group_by_date_id(start_date_id=start_date_id, end_date_id=end_date_id)
 
-    data.eval('up_stock_ratio=up_stock_number/list_stock_number*100', inplace=True)
-    data['up_stock_ratio'] = data['up_stock_ratio'].apply(np.round, decimals=2)
+    statistic_data.eval('up_ratio=up_stock_number/list_stock_number*100', inplace=True)
+    statistic_data['up_ratio'] = statistic_data['up_ratio'].apply(np.round, decimals=2)
+    data = statistic_data[['cal_date', 'up_ratio']]
 
-    text = data[['cal_date', 'up_stock_ratio']].to_string(index=False)
-    msgs = []
-    msgs.append(MIMEText(text, 'plain', 'utf-8'))
-
-    data.sort_values(by='cal_date', ascending=True, inplace=True)
-    data.reset_index(inplace=True)
-
-    holdings = get_holdings(data['up_stock_ratio'])
+    statistic_data.sort_values(by='cal_date', ascending=True, inplace=True)
+    statistic_data.reset_index(inplace=True)
+    holdings = get_holdings(statistic_data['up_ratio'])
     buy, sell = get_buy_sell_points(holdings)
 
     fig, ax = plt.subplots(2, 1, figsize=(16, 16), sharex=True)
 
-    index_daily = DB.get_index_daily(ts_code='000001.SH', start_date_id=start_date_id, end_date_id=end_date_id)
+    index_daily = DB.get_index_daily(ts_code=index_code, start_date_id=start_date_id, end_date_id=end_date_id)
+    index_daily['pct_chg'] = round(index_daily['pct_chg'], 1)
     gp = index_daily.groupby('ts_code')
     for ts_code, group_data in gp:
         group_data = group_data.set_index('cal_date')
-        group_data = group_data.reindex(data['cal_date'], method='ffill')
+        data = data.join(group_data[['pct_chg', 'close']], on='cal_date')
+        # group_data = group_data.reindex(data['cal_date'], method='ffill')
+        group_data.sort_values(by='cal_date', ascending=True, inplace=True)
         group_data = group_data.reset_index()
         adj_index = group_data['close'] / group_data.iloc[0]['close']
         cum_return_set = get_cum_return_rate(group_data['close'], holdings)
@@ -56,9 +58,9 @@ def execute(start_date='', end_date=''):
         ax1 = ax[1]
         ax1.plot(cum_return_set, label=ts_code + '=' + str(round(cum_return_set[-1], 2)))
 
-    max_loc = len(data) - 1
+    max_loc = len(statistic_data) - 1
     xticks_loc = [round(i / 14 * max_loc) for i in np.arange(0, 15)]
-    plt.xticks(xticks_loc, data.iloc[xticks_loc]['cal_date'], rotation=60)
+    plt.xticks(xticks_loc, statistic_data.iloc[xticks_loc]['cal_date'], rotation=60)
 
     ax0_0 = ax[0]
     # ax0_0.legend(loc=2, ncol=4)
@@ -81,7 +83,7 @@ def execute(start_date='', end_date=''):
     # }
     color = 'tab:red'
     ax0_1 = ax[0].twinx()
-    ax0_1.plot(data['up_stock_ratio'], color=color, linestyle='-')
+    ax0_1.plot(statistic_data['up_ratio'], 'ro-')
     ax0_1.yaxis.set_ticks(list(ratio_lable.keys()))
     ax0_1.yaxis.set_ticklabels(list(ratio_lable.values()))
     ax0_1.tick_params(axis='y', labelcolor=color)
@@ -99,6 +101,10 @@ def execute(start_date='', end_date=''):
     ax1.grid()
 
     plt.show()
+
+    text = data.to_string(index=False)
+    msgs = []
+    msgs.append(MIMEText(text, 'plain', 'utf-8'))
 
     fig.savefig('threshold_picture.png')
     image_msg = MIMEImage(open('threshold_picture.png', 'rb').read())
