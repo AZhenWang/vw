1f2166ed1e005a3be4b0d57d5e5a8a5da09706be
王珍 <865820954@qq.com>
2019-06-27 22:03:02 +0800

    s

--- app/batches/moneyflow.py
+++ app/batches/moneyflow.py
@@ -27,45 +27,46 @@ def execute(start_date='', end_date=''):
     start_date_id = pre_trade_cal.iloc[0]['date_id']
     end_date_id = trade_cal.iloc[-1]['date_id']
 
-    code_id = ''
-    flow = DB.get_moneyflows(code_id=code_id, end_date_id=end_date_id, start_date_id=start_date_id)
-    gp = flow.groupby(by='code_id')
+    codes = DB.get_latestopendays_code_list(
+        latest_open_days=window, date_id=trade_cal.iloc[0]['date_id'])
+    DB.truncate_mv_moneyflow()
+    code_ids = codes['code_id']
     new_rows = pd.DataFrame(columns=fields_map['mv_moneyflow'])
     i = 0
-    DB.truncate_mv_moneyflow()
-    for code_id, group_data in gp:
-        flow_mean = group_data[
+    for code_id in code_ids.values:
+        flow = DB.get_moneyflows(code_id=code_id, end_date_id=end_date_id, start_date_id=start_date_id)
+        flow_mean = flow[
             ['net_mf_vol', 'sell_elg_vol', 'buy_elg_vol', 'sell_lg_vol', 'buy_lg_vol', 'sell_md_vol',
              'buy_md_vol', 'sell_sm_vol', 'buy_sm_vol']].rolling(window=window).mean()
 
-        net_mf = (flow_mean['net_mf_vol']) * 100 /group_data['float_share']
+        net_mf = (flow_mean['net_mf_vol']) * 100 /flow['float_share']
         net_mf.name = 'net_mf'
 
-        mv_buy_elg = (flow_mean['buy_elg_vol']) * 100 / group_data['float_share']
+        mv_buy_elg = (flow_mean['buy_elg_vol']) * 100 / flow['float_share']
         mv_buy_elg.name = 'mv_buy_elg'
 
-        mv_sell_elg = (flow_mean['sell_elg_vol']) * 100 / group_data['float_share']
+        mv_sell_elg = (flow_mean['sell_elg_vol']) * 100 / flow['float_share']
         mv_sell_elg.name = 'mv_sell_elg'
 
-        net_elg = (flow_mean['buy_elg_vol'] - flow_mean['sell_elg_vol']) * 100 / group_data['float_share']
+        net_elg = (flow_mean['buy_elg_vol'] - flow_mean['sell_elg_vol']) * 100 / flow['float_share']
         net_elg.name = 'net_elg'
-        net_sm = (flow_mean['buy_sm_vol'] - flow_mean['sell_sm_vol']) * 100 / group_data['float_share']
+        net_sm = (flow_mean['buy_sm_vol'] - flow_mean['sell_sm_vol']) * 100 / flow['float_share']
         net_sm.name = 'net_sm'
-        net_md = (flow_mean['buy_md_vol'] - flow_mean['sell_md_vol']) * 100 / group_data['float_share']
+        net_md = (flow_mean['buy_md_vol'] - flow_mean['sell_md_vol']) * 100 / flow['float_share']
         net_md.name = 'net_md'
-        net_lg = (flow_mean['buy_lg_vol'] - flow_mean['sell_lg_vol']) * 100 / group_data['float_share']
+        net_lg = (flow_mean['buy_lg_vol'] - flow_mean['sell_lg_vol']) * 100 / flow['float_share']
         net_lg.name = 'net_lg'
 
-        turnover_rate_f = group_data['turnover_rate_f'] * (group_data['close'] - group_data['open']) / abs(
-            group_data['close'] - group_data['open'])
-        turnover_rate_back = group_data['turnover_rate_f'] * group_data['pct_chg'] / abs(
-            group_data['pct_chg'])
+        turnover_rate_f = flow['turnover_rate_f'] * (flow['close'] - flow['open']) / abs(
+            flow['close'] - flow['open'])
+        turnover_rate_back = flow['turnover_rate_f'] * flow['pct_chg'] / abs(
+            flow['pct_chg'])
         turnover_rate_f.fillna(value=turnover_rate_back, inplace=True)
         turnover_rate_f.name = 'turnover_rate_f'
         mv_turnover_rate_f = turnover_rate_f.rolling(window=5).mean()
         mv_turnover_rate_f.name = 'mv_turnover_rate_f'
-        turnover_rate_f2 = group_data['turnover_rate_f'] * (2 * group_data['close'] - group_data['high'] - group_data['low']) / abs(
-            group_data['high'] - group_data['low'])
+        turnover_rate_f2 = flow['turnover_rate_f'] * (2 * flow['close'] - flow['high'] - flow['low']) / abs(
+            flow['high'] - flow['low'])
         turnover_rate_f2.name = 'turnover_rate_f2'
         mv_turnover_rate_f2 = turnover_rate_f2.rolling(window=5).mean()
         mv_turnover_rate_f2.name = 'mv_turnover_rate_f2'
@@ -103,5 +104,5 @@ def execute(start_date='', end_date=''):
 
             }
             i += 1
-    if not new_rows.empty:
-        new_rows.to_sql('mv_moneyflow', DB.engine, index=False, if_exists='append', chunksize=1000)
+        if not new_rows.empty:
+            new_rows.to_sql('mv_moneyflow', DB.engine, index=False, if_exists='append', chunksize=1000)
