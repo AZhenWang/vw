443907080eb6dd6d2ba03997d633a9353ea9e85c
王珍 <865820954@qq.com>
2019-07-22 15:10:17 +0800

    s

--- app/fetcher/ts.py
+++ app/fetcher/ts.py
@@ -238,11 +238,9 @@ class Ts(Interface):
         if not new_rows.empty:
             existed_reports = DB.get_existed_reports(table_name=api, ts_code=ts_code, report_type=report_type, start_date=start_date, end_date=end_date)
 
-            print('new_rows0=', new_rows)
             if not existed_reports.empty:
                 new_rows = new_rows[~new_rows['end_date'].isin(existed_reports['end_date'])]
-            print('existed_reports=', existed_reports['end_date'])
-            print('new_rows1=', new_rows)
+                new_rows.drop_duplicate('end_date', inplace=True)
             new_rows = new_rows.merge(self.all_dates, left_on='ann_date', right_on='cal_date')
             new_rows = self.code_list.merge(new_rows, on='ts_code')
             avail_recorders = new_rows[fields_map[api]]
