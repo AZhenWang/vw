a45a606e8036ca7b88cee64cc659f1b784c91a3c
王珍 <865820954@qq.com>
2019-07-23 12:03:50 +0800

    s

--- app/fetcher/ts.py
+++ app/fetcher/ts.py
@@ -218,9 +218,9 @@ class Ts(Interface):
     def query_finance(self, api, report_type=''):
         # 按trade_date依次拉取所有股票信息
         # codes = self.code_list['ts_code']
-        # codes = ['000892.SZ']
+        codes = ['000892.SZ']
         # codes = ['600776.SH']
-        codes = ['000333.SZ']
+        # codes = ['000333.SZ']
         # codes = ['600776.SH', '000001.SZ', '000651.SZ']
         for ts_code in codes:
             flag = True
