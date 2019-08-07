c9c541fa6d0165372523846a2416ecd254de6658
王珍 <865820954@qq.com>
2019-08-07 19:01:12 +0800

    s

--- app/batches/fetch_quarterly.py
+++ app/batches/fetch_quarterly.py
@@ -24,9 +24,9 @@ def execute(start_date='', end_date=''):
         # for api in ts_apis:
         #     worker.query_finance(api, report_type='1')
 
-        # ts_apis = ['income', 'cashflow']
-        # for api in ts_apis:
-        #     worker.query_finance(api, report_type='2')
+        ts_apis = ['income', 'cashflow']
+        for api in ts_apis:
+            worker.query_finance(api, report_type='2')
         #
 
         # ts_apis = ['fina_indicator']
@@ -41,7 +41,7 @@ def execute(start_date='', end_date=''):
         # for api in ts_apis:
         #     worker.query_fina_mainbz(api)
 
-        ts_apis = ['dividend']
-        for api in ts_apis:
-            worker.query_by_ann_date(api)
+        # ts_apis = ['dividend']
+        # for api in ts_apis:
+        #     worker.query_by_ann_date(api)
         #
