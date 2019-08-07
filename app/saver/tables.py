3b3bf54eac185e10d34e8179790d0c867c01f576
王珍 <865820954@qq.com>
2019-08-07 10:04:43 +0800

    s

--- app/saver/tables.py
+++ app/saver/tables.py
@@ -155,8 +155,7 @@ fields_map = {
 
     'dividend': [
         'code_id', 'date_id', 'end_date', 'div_proc', 'stk_div', 'stk_bo_rate', 'stk_co_rate', 'cash_div',
-        'cash_div_tax', 'record_date', 'ex_date', 'pay_date', 'div_listdate', 'imp_ann_date',
-        'base_date', 'base_share'
+        'cash_div_tax', 'record_date', 'ex_date', 'pay_date', 'div_listdate', 'imp_ann_date'
     ],
 
     'comp_sys': [
