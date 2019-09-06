b71379f728cbd81b499cd8be3335f8c1f98ebb89
王珍 <865820954@qq.com>
2019-09-06 11:15:09 +0800

    s

--- app/batches/recommend_fina.py
+++ app/batches/recommend_fina.py
@@ -12,17 +12,20 @@ def execute(start_date='', end_date=''):
     :return:
     """
     new_rows = pd.DataFrame(columns=['code_id', 'comp_type',
-        'end_date', 'adj_close', 'total_mv', 'holdernum', 'holdernum_inc',
-        'roe', 'roe_mv', 'roe_std', 'op_pct',  'mix_op_diff',
-        'pe', 'pb', 'i_debt', 'share_ratio', 'IER', 'capital_turn', 'oper_pressure', 'OPM',
-        'X1', 'X2', 'X3', 'X4', 'X5', 'Z',
-        'dyr', 'dyr_or', 'dyr_mean',
-        'freecash_mv','cash_gap', 'cash_gap_r',  'receiv_pct',
-        'equity_pct', 'fix_asset_pct', 'rev_pct',
-        'income_rate',  'tax_rate', 'income_pct', 'tax_pct', 'tax_payable_pct', 'def_tax_ratio',
-        'dpba_of_gross', 'dpba_of_assets', 'rd_exp_or',
-        'V', 'dpd_V', 'pp', 'dpd_RR',
-        'LP', 'MP', 'HP', 'win_return', 'lose_return', 'odds',
+                                     'end_date', 'adj_close', 'total_mv', 'holdernum', 'holdernum_inc',
+                                     'roe', 'roe_mv', 'roe_std', 'op_pct', 'mix_op_diff',
+                                     'V', 'dpd_V', 'pp', 'pp0', 'pp_tax', 'pp_rd', 'pp_min', 'dpd_RR',
+                                     'pe', 'pb', 'i_debt', 'share_ratio', 'IER', 'capital_turn', 'oper_pressure', 'OPM',
+                                     'X1', 'X2', 'X3', 'X4', 'X5', 'Z',
+                                     'dyr', 'dyr_or', 'dyr_mean',
+                                     'freecash_mv', 'cash_gap', 'cash_gap_r', 'receiv_pct',
+                                     'equity_pct', 'fix_asset_pct', 'rev_pct',
+                                     'income_rate', 'tax_rate', 'income_pct', 'tax_pct', 'tax_payable_pct',
+                                     'def_tax_ratio',
+                                     'dpba_of_gross', 'dpba_of_assets', 'rd_exp_or',
+                                     'rev_pctmv', 'liab_pctmv', 'income_pctmv', 'tax_payable_pctmv', 'min_pctmv',
+                                     'equity_pctmv', 'fix_asset_pctmv',
+                                     'LP', 'MP', 'HP', 'win_return', 'lose_return', 'odds', 'adj_factor',
         'years', 'result'])
 
     # codes = DB.get_code_list(list_status='')
