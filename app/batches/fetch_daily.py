4adb7e38bdf751ccbcd18deacafe5467722836cb
王珍 <865820954@qq.com>
2019-09-09 09:43:21 +0800

    s

--- app/batches/fetch_daily.py
+++ app/batches/fetch_daily.py
@@ -17,18 +17,17 @@ def execute(start_date='', end_date=''):
     worker.set_trade_dates()
 
     if not worker.trade_dates.empty:
-        ts_apis = ['block_trade']
-        # ts_apis = ['daily', 'daily_basic', 'adj_factor',
-        #            'moneyflow','weekly', 'monthly', 'block_trade' ]
+        ts_apis = ['daily', 'daily_basic', 'adj_factor',
+                   'moneyflow', 'weekly', 'monthly', 'block_trade' ]
         for api in ts_apis:
             worker.query(api)
 
-        # idx_apis = ['index_daily', 'index_dailybasic']
-        # for idx_api in idx_apis:
-        #     worker.query_index(idx_api)
-        #
-        # fut_apis = ['fut_daily']
-        # for fut_api in fut_apis:
-        #     worker.query_fut(fut_api)
+        idx_apis = ['index_daily', 'index_dailybasic']
+        for idx_api in idx_apis:
+            worker.query_index(idx_api)
+
+        fut_apis = ['fut_daily']
+        for fut_api in fut_apis:
+            worker.query_fut(fut_api)
 
 
