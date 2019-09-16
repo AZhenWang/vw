d0c6cd8723cb4929c8d108325647458b48ce1bc0
王珍 <865820954@qq.com>
2019-09-16 12:33:17 +0800

    s

--- app/batches/recommend_fina.py
+++ app/batches/recommend_fina.py
@@ -12,7 +12,7 @@ def execute(start_date='', end_date=''):
     :return:
     """
     new_rows = pd.DataFrame(columns=['code_id', 'comp_type', 'end_date',
-                                     'f_ann_date', 'adj_close', 'total_mv', 'holdernum', 'holdernum_inc', 'holder_unit',
+                                     'f_ann_date', 'total_mv',  'adj_close', 'price_pct', 'holdernum', 'holdernum_inc', 'holdernum_2inc', 'holder_unit',
                                      'roe', 'roe_sale', 'roe_mv', 'roe_std', 'roe_adj', 'roe_sale_mv', 'op_pct',
                                      'mix_op_diff',
                                      'V', 'V_adj', 'V_sale', 'V_tax', 'dpd_V', 'pp', 'pp_adj', 'pp_sale', 'pp_tax',
@@ -28,22 +28,27 @@ def execute(start_date='', end_date=''):
                                      'dpba_of_gross', 'dpba_of_assets', 'rd_exp_or',
                                      'rev_pctmv', 'total_assets_pctmv', 'total_turn_pctmv', 'liab_pctmv',
                                      'income_pctmv', 'tax_payable_pctmv', 'equity_pctmv', 'fix_asset_pctmv',
-                                     'LP', 'MP', 'HP', 'win_return', 'lose_return', 'odds', 'adj_factor',
+                                     'LP', 'MP', 'HP', 'win_return', 'lose_return', 'odds', 'v_inc', 'adj_factor',
         'flag', 'step', 'nice', 'years', 'result', 'return_yearly'])
 
     # codes = DB.get_code_list(list_status='')
     # code_ids = codes['code_id']
-    code_ids = range(1, 500)
+    # code_ids = range(1, 500)
     # code_ids = range(2920, 3670)
     # code_ids = [range(1, 500), range(2920, 3670)]
-    # code_ids = [132]
+    code_ids = range(1, 3670)
+    # code_ids = [2942, 2144, 3044, 2, 1768]
+    # code_ids = [878]
     for code_id in code_ids:
-        # DB.delete_code_logs(code_id, tablename='fina_recom_logs')
+        DB.delete_code_logs(code_id, tablename='fina_recom_logs')
         logs = Fina.get_report_info(code_id=code_id, start_date=start_date, end_date=end_date, TTB='fina_sys',
                                     end_date_type='%1231%')
+        logs['price_pct'] = round(logs['adj_close'].pct_change() * 100, 2)
+        logs['v_inc'] = round(logs['MP'].pct_change() * 100, 2)
         logs.dropna(inplace=True)
         logs.reset_index(inplace=True, drop=True)
-
+        audits = Fina.get_report_info(code_id=code_id, start_date=start_date, end_date=end_date, TTB='fina_audit')
+        logs = logs.join(audits[['end_date', 'audit_result', 'audit_fees', 'audit_agency']].set_index('end_date'), on='end_date')
         Y = logs['adj_close']
         point_args = np.diff(np.where(np.diff(Y) > 0, 0, 1))
         logs = logs.join(pd.Series(point_args, name='point').shift())
@@ -55,7 +60,7 @@ def execute(start_date='', end_date=''):
 
             if log['liab_pctmv'] < log['rev_pctmv'] * 1.5 \
                     and log['equity_pctmv'] > 18 and log['fix_asset_pctmv'] > -10 and log['total_assets_pctmv'] > 10 and log['tax_payable_pctmv'] > 5 \
-                    and log['receiv_pct'] < 20 and log['Z'] > 0.8 \
+                    and log['receiv_pct'] < 25 and log['Z'] > 0.8 \
                     and log['cash_act_in'] > 8 \
                     and log['i_debt'] < 50 \
                     and log['roe_mv'] > 12 \
@@ -80,21 +85,29 @@ def execute(start_date='', end_date=''):
 
             # 三看年报性价比
             nice = 0
-            if log['pp_adj'] > 1\
+            if log['pp_adj'] > 0.5\
                     and log['pp'] > 1.3 \
                     and log['pp_sale'] > 1.5 \
                     and log['dpd_RR'] > 2:
                 nice = 1
-            elif log['pp_adj'] <= 0.6 \
+            elif log['pp_adj'] <= 0.5 \
                 or log['pp'] < 1 \
                 or log['pp_sale'] < 1 \
                 or log['dpd_RR'] < 1:
                 nice = -1
 
+            holdernum_2inc = log['holdernum_inc']
+            if j >= 1 and logs.iloc[j-1]['holdernum_inc'] * holdernum_2inc > 0:
+                holdernum_2inc += logs.iloc[j-1]['holdernum_inc']
+
+            holder_unit = round(log['total_mv'] / log['holdernum'], 1)
             logs.at[index, 'flag'] = flag
             logs.at[index, 'step'] = step
             logs.at[index, 'nice'] = nice
-            logs.at[index, 'holder_unit'] = round(log['total_mv'] / log['holdernum'], 1)
+            logs.at[index, 'holder_unit'] = holder_unit
+            logs.at[index, 'price_pct'] = log['price_pct']
+            logs.at[index, 'holdernum_2inc'] = holdernum_2inc
+            logs.at[index, 'v_inc'] = log['v_inc']
 
             today = logs.iloc[j]['end_date']
             later_logs = logs[logs.end_date > today]
@@ -111,8 +124,9 @@ def execute(start_date='', end_date=''):
                 logs.at[index, 'return_yearly'] = round((result**(1/years) - 1)*100, 2)
 
         new_rows = pd.concat([new_rows, logs], sort=False)
-        # print(new_rows[['end_date', 'adj_close', 'holder_unit', 'odds', 'rev_pct', 'equity_pct', 'roe', 'roe_mv', 'roe_sale', 'roe_sale_mv', 'flag', 'step', 'nice', 'result', 'years', 'return_yearly']])
+        # print(new_rows[['end_date', 'adj_close', 'holder_unit', 'price_pct', 'holdernum_inc', 'holdernum_2inc', 'flag', 'step', 'nice', 'result', 'years', 'return_yearly', ]])
         # os.ex
+
     if not new_rows.empty:
         new_rows.to_sql('fina_recom_logs', DB.engine, index=False, if_exists='append', chunksize=1000)
 
