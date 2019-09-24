33c6be90ce8b13ecf3db9c06ade5fac2dfe1f975
王珍 <865820954@qq.com>
2019-09-24 17:34:44 +0800

    s

--- app/batches/fina_super.py
+++ app/batches/fina_super.py
@@ -22,7 +22,8 @@ def execute(start_date='', end_date=''):
     new_rows = pd.DataFrame(columns=fields_map['fina_super'])
     # code_ids = [214, 2381]
     # code_ids = [2555, 214, 2, 132, 73, 2381]
-    # code_ids = [2381]
+    # code_ids = [1486, 214, 2, 13, 161, 2381, 3012]
+    # code_ids = [214]
     # code_ids = range(2920, 3670)
     code_ids = range(1, 3668)
     for code_id in code_ids:
@@ -35,10 +36,11 @@ def execute(start_date='', end_date=''):
         dailys = DB.get_code_info(code_id=code_id, start_date=init_start_date, end_date=end_date, TTB='daily')
         logs['report_adj_factor'] = logs['adj_factor']
         logs['report_adj_close'] = logs['adj_close']
-        base = trade_cal.join(logs[['f_ann_date', 'end_date', 'LLP', 'LP', 'MP', 'HP', 'HHP', 'MP_pct', 'roe_mv', 'report_adj_factor', 'V', 'V_adj', 'V_sale', 'V_ebitda', 'dpd_V', 'pe', 'pb', 'report_adj_close']].set_index('f_ann_date', drop=False), on='cal_date')
-        base = base.join(dailys[['close', 'high', 'low', 'open', 'adj_factor', 'total_mv']], on='date_id')
+        base = trade_cal.join(logs[['f_ann_date', 'end_date', 'LLP', 'LP', 'MP', 'HP', 'HHP', 'MP_pct', 'roe_mv', 'roe_sale_mv', 'report_adj_factor', 'V', 'V_adj', 'V_rd', 'V_sale', 'V_ebitda', 'dpd_V', 'report_adj_close']].set_index('f_ann_date', drop=False), on='cal_date')
+        base = base.join(dailys[['close', 'high', 'low', 'open', 'adj_factor', 'total_mv', 'pb', 'pe']], on='date_id')
         base.fillna(method='ffill', inplace=True)
-        base.dropna(inplace=True)
+        base.dropna(subset=['report_adj_close', 'V', 'V_sale', 'V_ebitda'], inplace=True)
+
         base = base.join(recom_logs[['f_ann_date', 'flag', 'step', 'nice', 'holdernum_2inc']].set_index('f_ann_date'),
                          on='cal_date')
         base.fillna(method='ffill', inplace=True)
@@ -47,30 +49,13 @@ def execute(start_date='', end_date=''):
         adj_close = base['close'] * base['adj_factor']
         adj_high = base['high'] * base['adj_factor']
         adj_low = base['low'] * base['adj_factor']
-        d_dates = pd.Series(index=base.index)
-        for i in range(len(base)):
-            sd = datetime.strptime(base.iloc[i]['f_ann_date'], '%Y%m%d')
-            ed = datetime.strptime(base.iloc[i]['cal_date'], '%Y%m%d')
-            d_dates.iloc[i] = (ed-sd).days
-
-        had_benefit = (1 + base['roe_mv'] /100 * d_dates / 365)
-
-        adj_LLP = round(base['LLP'], 2)
-        adj_LP = round(base['LP'], 2)
-        adj_MP = round(had_benefit * base['MP'], 2)
-        adj_HP = round(base['HP'], 2)
-        adj_HHP = round(base['HHP'], 2)
-
-        LP = round(adj_LP / base['adj_factor'], 2)
-        MP = round(adj_MP / base['adj_factor'], 2)
-        HP = round(adj_HP / base['adj_factor'], 2)
-
-
-        # win_base = pd.concat([adj_HP, adj_close], axis=1).min(axis=1)
-        # lose_base = pd.concat([adj_LP, adj_close], axis=1).min(axis=1)
-        # win_return = round((adj_HP - adj_close) * 100 / win_base, 2)
-        # lose_return = round((adj_LP - adj_close) * 100 / lose_base, 2)
-        # odds = round(win_return + lose_return, 2)
+        # d_dates = pd.Series(index=base.index)
+        # for i in range(len(base)):
+        #     sd = datetime.strptime(base.iloc[i]['f_ann_date'], '%Y%m%d')
+        #     ed = datetime.strptime(base.iloc[i]['cal_date'], '%Y%m%d')
+        #     d_dates.iloc[i] = (ed-sd).days
+        #
+        # had_benefit = (1 + base['roe_sale_mv'] /100 * d_dates / 365)
 
         data = pd.DataFrame()
         data['end_date'] = base['end_date']
@@ -79,42 +64,62 @@ def execute(start_date='', end_date=''):
         data['code_id'] = code_id
         data['total_mv'] = base['total_mv']
         data['adj_factor'] = base['adj_factor']
+
+        pb = base['pb']
+        pe = base['pe']
+        data['pp'] = round(base['V'] / pb, 2)
+        data['pp_adj'] = round(base['V_adj'] / pb, 2)
+        data['pp_rd'] = round(base['V_rd'] / pb, 2)
+        data['pp_sale'] = round(base['V_sale'] / pb, 2)
+        data['pp_ebitda'] = round(base['V_ebitda'] / pb, 2)
+        data['pp_comp'] = round((data['pp'] + data['pp_ebitda']) / 2, 2)
+        data['dpd_RR'] = round(base['dpd_V'] / pe, 2)
+
+        adj_MP = round(adj_close * data['pp_sale'], 2)
+        adj_MP = round(adj_MP, 2)
+        adj_LP = round(adj_MP / 2, 2)
+        adj_HP = round(2 * adj_MP, 2)
+
+        win = (2 * (adj_close * data['pp']) - adj_close) / adj_close
+        lose = ((adj_close * data['pp']) / 2 - adj_close) / adj_close
+        odds_pp = round(((1 + win) * (1 + lose) - 1) * 100, 1)
+
+        win_return2 = (base['HHP'] - adj_close) / adj_close
+        lose_return2 = (base['LLP'] -adj_close) / adj_close
+        odds2 = round(((1 + win_return2) * (1 + lose_return2) - 1) * 100, 1)
+
+        LP_max = adj_LP/0.9
+        HP_min = adj_HP/1.1
+        LP_min = adj_LP
+        HP_max = adj_HP
+
+        win_return = (HP_min - adj_close) / adj_close
+        lose_return = (LP_min - adj_close) / adj_close
+        odds = round(((1 + win_return) * (1 + lose_return) - 1) * 100, 1)
+        win_return = round(win_return * 100, 1)
+        lose_return = round(lose_return * 100, 1)
+
+        LP = round(adj_LP / base['adj_factor'], 2)
+        MP = round(adj_MP / base['adj_factor'], 2)
+        HP = round(adj_HP / base['adj_factor'], 2)
+
+
         data['LP'] = LP
         data['MP'] = MP
         data['HP'] = HP
-        data['adj_LLP'] = adj_LLP
+        data['adj_LLP'] = base['LLP']
         data['adj_LP'] = adj_LP
         data['adj_MP'] = adj_MP
         data['adj_HP'] = adj_HP
-        data['adj_HHP'] = adj_HHP
+        data['adj_HHP'] = base['HHP']
         data['MP_pct'] = base['MP_pct']
-
-        LP_level = pd.concat([data['adj_LLP'], data['adj_LP']/0.9], axis=1)
-        HP_level = pd.concat([data['adj_HHP'], data['adj_HP']/1.1], axis=1)
-        LP_max = LP_level.max(axis=1)
-        LP_min = LP_level.min(axis=1)
-        HP_max = HP_level.max(axis=1)
-        HP_min = HP_level.min(axis=1)
-
-        win_base = pd.concat([HP_min, adj_close], axis=1).min(axis=1)
-        lose_base = pd.concat([LP_min, adj_close], axis=1).min(axis=1)
-        win_return = round((HP_min - adj_close) * 100 / win_base, 2)
-        lose_return = round((LP_min - adj_close) * 100 / lose_base, 2)
-        odds = round(win_return + lose_return, 2)
 
         data['adj_close'] = round(base['close'] * base['adj_factor'], 2)
         data['win_return'] = win_return
         data['lose_return'] = lose_return
         data['odds'] = odds
-
-        pb = base['pb'] * adj_close / base['report_adj_close']
-        pe = base['pe'] * adj_close / base['report_adj_close']
-        data['pp'] = round(base['V'] / pb, 2)
-        data['pp_adj'] = round(base['V_adj'] / pb, 2)
-        data['pp_sale'] = round(base['V_sale'] / pb, 2)
-        data['pp_ebitda'] = round(base['V_ebitda'] / pb, 2)
-        data['pp_comp'] = round((data['pp'] + data['pp_ebitda'])/2, 2)
-        data['dpd_RR'] = round(base['dpd_V'] / pe, 2)
+        data['odds2'] = odds2
+        data['odds_pp'] = odds_pp
 
         data['flag'] = base['flag']
         data['step'] = base['step']
@@ -155,7 +160,8 @@ def execute(start_date='', end_date=''):
         data['basis'] = basis
 
         data[data.isin([np.inf, -np.inf])] = np.nan
-        data.dropna(inplace=True)
+        data.fillna(0, inplace=True)
+        # data.dropna(inplace=True)
         # new_rows = pd.concat([new_rows, data.reset_index(drop=True)], sort=False)
         if not data.empty:
             data.to_sql('fina_super', DB.engine, index=False, if_exists='append', chunksize=1000)
