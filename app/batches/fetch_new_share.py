f5cf9d60fa368f0af4a429c5152f13ccd1383e9b
王珍 <865820954@qq.com>
2019-09-26 19:29:17 +0800

    s

--- app/batches/fetch_new_share.py
+++ app/batches/fetch_new_share.py
@@ -17,17 +17,17 @@ def execute(start_date='', end_date=''):
     worker.update_trade_cal()
     worker.set_trade_dates()
 
-    # ts_apis = ['income', 'cashflow', 'balancesheet']
-    # for api in ts_apis:
-    #     worker.query_finance(api, report_type='1')
-    #
-    # ts_apis = ['income', 'cashflow']
-    # for api in ts_apis:
-    #     worker.query_finance(api, report_type='2')
-    #
-    # ts_apis = ['fina_indicator']
-    # for api in ts_apis:
-    #     worker.query_finance(api=api, need_fields=True)
+    ts_apis = ['income', 'cashflow', 'balancesheet']
+    for api in ts_apis:
+        worker.query_finance(api, report_type='1')
+
+    ts_apis = ['income', 'cashflow']
+    for api in ts_apis:
+        worker.query_finance(api, report_type='2')
+
+    ts_apis = ['fina_indicator']
+    for api in ts_apis:
+        worker.query_finance(api=api, need_fields=True)
 
     ts_apis = ['fina_audit', 'stk_holdernumber']
     for api in ts_apis:
