2c977fefdda49622344dba904d30420fb49f8dd6
王珍 <865820954@qq.com>
2019-10-24 15:48:42 +0800

    s

--- app/batches/fetch_quarterly.py
+++ app/batches/fetch_quarterly.py
@@ -9,33 +9,29 @@ def execute(start_date='', end_date=''):
     worker.update_trade_cal()
     worker.set_trade_dates()
 
-    ts_apis = ['cashflow']
-    for api in ts_apis:
-        worker.query_finance(api, report_type='1')
-
     # ts_apis = ['balancesheet', 'income', 'cashflow']
     # for api in ts_apis:
     #     worker.query_finance(api, report_type='1')
 
-    # ts_apis = ['income', 'cashflow']
-    # for api in ts_apis:
-    #     worker.query_finance(api, report_type='2')
-    #
-    # ts_apis = ['fina_indicator']
-    # for api in ts_apis:
-    #     worker.query_finance(api=api, need_fields=True)
-    #
-    # ts_apis = ['fina_audit', 'stk_holdernumber']
-    # for api in ts_apis:
-    #     worker.query_finance(api=api, need_fields=True)
-    #
-    # ts_apis = ['express']
-    # for api in ts_apis:
-    #     worker.query_finance(api=api, need_fields=True)
-    #
-    # ts_apis = ['fina_mainbz']
-    # for api in ts_apis:
-    #     worker.query_fina_mainbz(api)
+    ts_apis = ['income', 'cashflow']
+    for api in ts_apis:
+        worker.query_finance(api, report_type='2')
+
+    ts_apis = ['fina_indicator']
+    for api in ts_apis:
+        worker.query_finance(api=api, need_fields=True)
+
+    ts_apis = ['fina_audit', 'stk_holdernumber']
+    for api in ts_apis:
+        worker.query_finance(api=api, need_fields=True)
+
+    ts_apis = ['express']
+    for api in ts_apis:
+        worker.query_finance(api=api, need_fields=True)
+
+    ts_apis = ['fina_mainbz']
+    for api in ts_apis:
+        worker.query_fina_mainbz(api)
 
     # ts_apis = ['dividend']
     # for api in ts_apis:
