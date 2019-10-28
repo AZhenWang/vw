5fc07fc509ae1659192339012e5252c714cd9c49
王珍 <865820954@qq.com>
2019-10-28 13:16:53 +0800

    s

--- app/batches/fetch_quarterly.py
+++ app/batches/fetch_quarterly.py
@@ -21,7 +21,7 @@ def execute(start_date='', end_date=''):
     # for api in ts_apis:
     #     worker.query_finance(api=api, need_fields=True)
 
-    ts_apis = ['fina_audit']
+    ts_apis = ['stk_holdernumber']
     for api in ts_apis:
         worker.query_finance(api=api, need_fields=True)
 
