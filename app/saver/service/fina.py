8c4c1196673c8b10abdce94e55960d4fa991a976
王珍 <865820954@qq.com>
2019-10-31 19:39:26 +0800

    s

--- app/saver/service/fina.py
+++ app/saver/service/fina.py
@@ -126,10 +126,12 @@ class Fina(Base):
     @classmethod
     def delete_logs_in_end_dates(cls, end_dates, code_id='', tablename=''):
         if len(end_dates) >= 1:
+            print('进来了')
             pd.io.sql.execute(
                 'delete from ' + tablename + ' where code_id = ' + code_id + ' and end_date in (' + ('%s,' * len(end_dates)).strip(',') + ')',
                 cls.engine,
                 params=end_dates)
+            print('出去了')
 
     @classmethod
     def delete_fina_by_f_ann_date(cls, code_id='', start_date='', end_date='', tablename=''):
