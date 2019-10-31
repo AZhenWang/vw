7220b3a6435bbda62fa8ed4fda1049d8a84eccb2
王珍 <865820954@qq.com>
2019-10-31 18:45:31 +0800

    s

--- app/saver/service/fina.py
+++ app/saver/service/fina.py
@@ -48,7 +48,7 @@ class Fina(Base):
         return existed_reports
 
     @classmethod
-    def get_report_info(cls, code_id, start_date='', end_date='', TTB='', report_type='', end_date_type=''):
+    def get_report_info(cls, code_id, start_date_id='', end_date_id='', TTB='', report_type='', end_date_type=''):
         """
 
         :param code_id:
@@ -62,8 +62,8 @@ class Fina(Base):
         sql_str = ' SELECT  r.*' \
                   ' FROM ' + TTB + ' r ' \
                                    ' where r.code_id = :code_id ' \
-                                   ' and r.end_date >= :sdi and r.end_date <= :edi'
-        params = {'code_id': str(code_id), 'sdi': str(start_date), 'edi': str(end_date)}
+                                   ' and r.date_id >= :sdi and r.date_id <= :edi'
+        params = {'code_id': str(code_id), 'sdi': str(start_date_id), 'edi': str(end_date_id)}
 
         if report_type != '':
             sql_str += ' and r.report_type =:report_type'
@@ -106,6 +106,15 @@ class Fina(Base):
         return report_info
 
     @classmethod
+    def delete_logs_by_date_id(cls, code_id='', start_date_id='', end_date_id='', tablename=''):
+        if code_id == '':
+            pd.io.sql.execute('delete from ' + tablename + ' where date_id >= %s and date_id <= %s',
+                              cls.engine, params=[str(start_date_id), str(end_date_id)])
+        else:
+            pd.io.sql.execute('delete from ' + tablename + ' where code_id = %s and date_id >= %s and date_id <= %s',
+                              cls.engine, params=[str(code_id), str(start_date_id), str(end_date_id)])
+
+    @classmethod
     def delete_logs_by_end_date(cls, code_id='', start_date='', end_date='', tablename=''):
         if code_id == '':
             pd.io.sql.execute('delete from ' + tablename + ' where end_date >= %s and end_date <= %s',
@@ -115,25 +124,43 @@ class Fina(Base):
                               cls.engine, params=[str(code_id), str(start_date), str(end_date)])
 
     @classmethod
+    def delete_logs_in_end_dates(cls, end_dates, code_id='', tablename=''):
+        if len(end_dates) >= 1:
+            pd.io.sql.execute(
+                'delete from ' + tablename + ' where code_id = %s and end_date in (' + ('%s,' * len(end_dates)).strip(
+                    ',') + ')',
+                cls.engine,
+                params=[str(code_id), end_dates])
+
+    @classmethod
+    def delete_fina_by_f_ann_date(cls, code_id='', start_date='', end_date='', tablename=''):
+        if code_id == '':
+            pd.io.sql.execute('delete from ' + tablename + ' where f_ann_date >= %s and f_ann_date <= %s',
+                              cls.engine, params=[str(start_date), str(end_date)])
+        else:
+            pd.io.sql.execute('delete from ' + tablename + ' where code_id = %s and f_ann_date >= %s and f_ann_date <= %s',
+                              cls.engine, params=[str(code_id), str(start_date), str(end_date)])
+
+    @classmethod
     def delete_fina_super_logs(cls, code_id='', start_date='', end_date=''):
         pd.io.sql.execute('delete from fina_super where code_id = %s and cal_date >= %s and cal_date <= %s',
                           cls.engine, params=[str(code_id), str(start_date), str(end_date)])
 
     @classmethod
-    def get_divdends(cls, code_id, start_date, end_date):
+    def get_divdends(cls, code_id, start_date_id, end_date_id):
         sql = ' SELECT api.*, b.total_share  FROM dividend as api ' \
-              ' left join trade_cal tc on tc.cal_date = api.end_date ' \
+              ' left join trade_cal tc on tc.cal_date = api.pay_date ' \
               ' left join daily_basic b on b.code_id = api.code_id and b.date_id = tc.id' \
               ' where api.code_id = :ci ' \
-              ' and tc.cal_date >= :start_date and tc.cal_date <= :end_date' \
-              ' order by api.end_date asc'
+              ' and tc.id >= :sdi and tc.id <= :edi' \
+              ' order by api.pay_date asc'
 
-        params = {'ci': str(code_id), 'start_date': str(start_date), 'end_date': str(end_date)}
+        params = {'ci': str(code_id), 'sdi': str(start_date_id), 'edi': str(end_date_id)}
 
         existed_reports = pd.read_sql(
             sa.text(sql),
             cls.engine,
             params=params
         )
-
+        existed_reports['total_share'] = existed_reports['total_share'].fillna(method='ffill')
         return existed_reports
