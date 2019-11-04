0da429ebe36c2fdc2148314b9f736224e4b1bf97
王珍 <865820954@qq.com>
2019-11-04 21:54:19 +0800

    s

--- app/batches/recommend_fina.py
+++ app/batches/recommend_fina.py
@@ -11,19 +11,19 @@ def execute(start_date='', end_date=''):
     :param end_date: 公告结束日期
     :return:
     """
-    new_rows = pd.DataFrame(columns=['code_id', 'comp_type', 'end_date',
-                                     'f_ann_date', 'total_mv',  'holdernum', 'holdernum_inc', 'holdernum_2inc', 'holder_unit',
+    new_rows = pd.DataFrame(columns=['code_id', 'date_id', 'comp_type', 'end_date',
+                                     'total_mv',  'holdernum', 'holdernum_inc', 'holdernum_2inc', 'holder_unit',
                                      'roe',  'roe_rd', 'roe_sale', 'roe_mv', 'roe_std', 'roe_adj', 'roe_rd_mv', 'roe_sale_mv', 'op_pct',
                                      'mix_op_diff',
                                      'V', 'V_adj', 'V_rd', 'V_sale', 'V_tax', 'dpd_V', 'pp', 'pp_rd', 'pp_adj', 'pp_sale', 'pp_tax',
                                      'dpd_RR',
-                                     'pe', 'pb', 'i_debt', 'share_ratio', 'IER',
+                                     'pe', 'pb', 'EE', 'i_debt', 'share_ratio', 'IER',
                                      'oth_receiv_rate', 'money_cap_pct', 'st_borr_pct', 'st_borr_rate', 'capital_turn', 'oper_pressure', 'OPM',
                                      'Z',
                                      'dyr', 'dyr_or', 'dyr_mean',
                                      'freecash_mv', 'cash_gap', 'cash_gap_r', 'receiv_pct', 'receiv_rate', 'cash_act_in',
                                      'cash_act_out', 'cash_act_rate',
-                                     'equity_pct', 'fix_asset_pct', 'revenue', 'rev_pct', 'gross_rate',
+                                     'equity_pct', 'fix_asset_pct', 'revenue', 'rev_pct', 'rev_pct_yearly', 'share_pct', 'gross_rate',
                                      'income_rate', 'tax_rate', 'income_pct', 'tax_pct', 'tax_payable_pct',
                                      'def_tax_ratio',
                                      'dpba_of_gross', 'dpba_of_assets', 'rd_exp_or',
@@ -39,25 +39,27 @@ def execute(start_date='', end_date=''):
     # code_ids = range(1, 500)
     # code_ids = range(2920, 3670)
     # code_ids = [range(1, 500), range(2920, 3670)]
-    # code_ids = range(1, 3820)
+    code_ids = range(1, 3868)
     # code_ids = range(3559, 3670)
     # code_ids = [3,56,63,65,78,94,103,104,110,111,115,132,138,156,171,177,198,208,213,220,239,250,256,259,284,290,297,309,311,313,324,330,333,337,390,396,399,410,422,431,450,453,454,460,465,466,468,471,483,484,486,494,501,502,508,518,525,532,556,560,563,566,571,573,582,589,600,609,626,629,630,637,662,683,687,690,710,714,716,725,738,750,757,780,789,795,804,812,823,824,828,833,845,849,853,856,862,870,871,872,874,875,885,887,895,896,898,900,905,913,916,918,939,960,1012,1013,1024,1028,1043,1046,1053,1059,1063,1065,1076,1094,1105,1106,1110,1115,1128,1131,1137,1142,1145,1150,1155,1170,1187,1196,1200,1206,1208,1211,1229,1239,1256,1258,1262,1263,1269,1273,1277,1278,1279,1313,1321,1322,1328,1342,1345,1347,1348,1353,1355,1360,1362,1368,1376,1383,1386,1389,1392,1398,1399,1402,1409,1413,1420,1422,1424,1432,1437,1443,1464,1468,1489,1490,1491,1492,1501,1503,1504,1514,1517,1524,1525,1529,1540,1553,1556,1563,1575,1580,1581,1585,1587,1595,1597,1614,1619,1620,1624,1625,1627,1628,1635,1636,1648,1654,1670,1675,1679,1695,1699,1707,1728,1738,1739,]
     # code_ids = [2352, 1348, 1376, 3304, 1707]
-    code_ids = [2352]
+    # code_ids = [1376]
     # code_ids = [1398, 2760,1386,1861,1707,2352,885,2344,2543,1524,1504,1975,2283,1392,1563,3242,2095,3192,3682,1273,1743,3315,1728,1783,]
     # code_ids = range(3800, 3820)
+    start_date_id = DB.get_date_id(start_date)
+    end_date_id = DB.get_date_id(end_date)
+
     for code_id in code_ids:
         print('code_id=', code_id)
         DB.delete_code_logs(code_id, tablename='fina_recom_logs')
-        logs = Fina.get_report_info(code_id=code_id, start_date=start_date, end_date=end_date, TTB='fina_sys',
-                                    end_date_type='%1231%')
+        logs = Fina.get_report_info(code_id=code_id, start_date_id=start_date_id, end_date_id=end_date_id, TTB='fina_sys')
         logs = logs.dropna(subset=['total_mv'])
         logs.fillna(0, inplace=True)
         if logs.empty:
             continue
         logs['price_pct'] = round(logs['adj_close'].pct_change() * 100, 2)
         logs.reset_index(inplace=True, drop=True)
-        audits = Fina.get_report_info(code_id=code_id, start_date=start_date, end_date=end_date, TTB='fina_audit')
+        audits = Fina.get_report_info(code_id=code_id, start_date_id=start_date_id, end_date_id=end_date_id, TTB='fina_audit')
         logs = logs.join(audits[['end_date', 'audit_result', 'audit_fees', 'audit_agency']].set_index('end_date'), on='end_date')
         Y = logs['adj_close']
         point_args = np.diff(np.where(np.diff(Y) > 0, 0, 1))
@@ -81,12 +83,14 @@ def execute(start_date='', end_date=''):
                 flag = 1
 
             elif log['receiv_pct'] > 25 or log['Z'] < 1 \
-                    or log['cash_act_in'] < -20 \
+                    or log['cash_act_in'] < 0 \
+                    or log['cash_act_out'] < 0 \
                     or (log['i_debt'] > 40 and log['IER'] < 8) \
                     or log['i_debt'] > 60 \
                     or log['roe_mv'] < 12 \
                     or log['IER'] < 3\
                     or log['oth_receiv_rate'] > 20 \
+                    or log['st_borr_rate'] > 40 \
                     or (log['st_borr_pct'] > 50 and log['money_cap_pct'] > 50):
                 flag = -1
 
@@ -105,10 +109,20 @@ def execute(start_date='', end_date=''):
                 pre_roe_mv = log['roe_mv']
                 pre_capital_turn = log['capital_turn']
 
-            if (log['roe_sale'] >= log['roe_sale_mv'] or log['roe_sale'] >= pre_roe_sale or log['roe_mv'] > pre_roe_mv)\
-                    and (log['roe'] >= log['roe_mv'] or log['roe'] >= pre_roe or log['roe_sale_mv'] > pre_roe_sale_mv)\
-                    and log['capital_turn'] > pre_capital_turn :
+            # if (log['roe_sale'] >= log['roe_sale_mv'] or log['roe_sale'] >= pre_roe_sale or log['roe_mv'] > pre_roe_mv)\
+            #         and (log['roe'] >= log['roe_mv'] or log['roe'] >= pre_roe or log['roe_sale_mv'] > pre_roe_sale_mv)\
+            #         and log['capital_turn'] > pre_capital_turn:
+            #     step = 1
+
+            if log['roe'] - pre_roe > 1 \
+                and log['rev_pct_yearly'] > 10 \
+                and log['capital_turn'] > pre_capital_turn:
+
                 step = 1
+            elif log['roe'] - pre_roe < -1 \
+                    and log['rev_pct'] < 0 \
+                    and log['capital_turn'] < pre_capital_turn:
+                step = -1
 
             # 三看年报性价比
             nice = 0
