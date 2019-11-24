a027f02beb75d53a7117575be6e22128484734b6
王珍 <865820954@qq.com>
2019-11-24 20:06:55 +0800

    s

--- app/fetcher/ts.py
+++ app/fetcher/ts.py
@@ -375,7 +375,7 @@ class Ts(Interface):
                 new_rows = new_rows.merge(self.all_dates, left_on='ann_date', right_on='cal_date')
                 new_rows = self.code_list.merge(new_rows, on='ts_code')
                 print(new_rows)
-                print(' len(new_rows)=',  len(new_rows))
+                print(' len(new_rows)=',  len(new_rows), ', new_rows.empty=', new_rows.empty)
                 Fina.delete_logs_in_end_dates(code_id=new_rows.iloc[0]['code_id'], end_dates=new_rows['end_date'], tablename=api)
                 avail_recorders = new_rows[fields_map[api]]
                 avail_recorders.to_sql(api, DB.engine, index=False, if_exists='append', chunksize=3000)
