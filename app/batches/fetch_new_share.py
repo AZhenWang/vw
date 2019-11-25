d647cd2345785081e6e1d2b26d012528f8e3dd9d
王珍 <865820954@qq.com>
2019-11-25 11:19:47 +0800

    s

--- app/batches/fetch_new_share.py
+++ app/batches/fetch_new_share.py
@@ -14,25 +14,29 @@ def execute(start_date='', end_date=''):
     result = worker.update_new_share(start_date=start_date, end_date=end_date)
     worker.update_stock_basic()
     worker.set_code_list()
-    worker.set_code_list(list_status='N')
+    # worker.set_code_list(list_status='N')
 
     worker.update_trade_cal()
     worker.set_trade_dates()
 
     if result:
-        ts_apis = ['income', 'cashflow', 'balancesheet']
+        ts_apis = ['income']
         for api in ts_apis:
             worker.query_finance(api, report_type='1')
 
-        ts_apis = ['income', 'cashflow']
-        for api in ts_apis:
-            worker.query_finance(api, report_type='2')
-
-        ts_apis = ['fina_indicator']
-        for api in ts_apis:
-            worker.query_finance(api=api, need_fields=True)
-
-        ts_apis = ['fina_audit', 'stk_holdernumber']
-        for api in ts_apis:
-            worker.query_finance(api=api, need_fields=True)
-
+        # ts_apis = ['income', 'cashflow', 'balancesheet']
+        # for api in ts_apis:
+        #     worker.query_finance(api, report_type='1')
+        #
+        # ts_apis = ['income', 'cashflow']
+        # for api in ts_apis:
+        #     worker.query_finance(api, report_type='2')
+        #
+        # ts_apis = ['fina_indicator']
+        # for api in ts_apis:
+        #     worker.query_finance(api=api, need_fields=True)
+        #
+        # ts_apis = ['fina_audit', 'stk_holdernumber']
+        # for api in ts_apis:
+        #     worker.query_finance(api=api, need_fields=True)
+        #
