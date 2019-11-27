b8bdceb55cdafa04350bfb232ea8365e834ea509
王珍 <865820954@qq.com>
2019-11-27 10:29:38 +0800

    s

--- app/fetcher/ts.py
+++ app/fetcher/ts.py
@@ -334,7 +334,7 @@ class Ts(Interface):
         # 按trade_date依次拉取所有股票信息
         # codes = self.code_list[self.code_list['code_id'] >= 3801]['ts_code']
         # codes = self.code_list['ts_code']
-        codes = ['688368.SH', '603390.SH']
+        codes = ['688368.SH']
         if need_fields != '':
             fields = fields_map[api].copy()
             fields.remove('code_id')
@@ -362,7 +362,9 @@ class Ts(Interface):
         else:
             new_rows = self.pro.query(api, ts_code=ts_code, fields=fields,  start_date=start_date, end_date=end_date, report_type=report_type)
         new_rows.drop_duplicates('end_date', inplace=True)
-        print('api=', api, new_rows[['end_date']])
+        print(self.all_dates)
+        print('api=', api, new_rows[['ann_date', 'end_date']])
+
         if not new_rows.empty:
             existed_reports = Fina.get_existed_reports(table_name=api, ts_code=ts_code, report_type=report_type, start_date=start_date, end_date=end_date)
             if not existed_reports.empty:
