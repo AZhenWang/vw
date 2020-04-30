7441b1570f113a5fba5a996716f1081c1be2d719
王珍 <865820954@qq.com>
2020-04-30 09:19:54 +0800

    s

--- app/fetcher/ts.py
+++ app/fetcher/ts.py
@@ -336,11 +336,12 @@ class Ts(Interface):
     def query_finance(self, api, report_type='', need_fields=''):
         # 按trade_date依次拉取所有股票信息
         # codes = self.code_list[self.code_list['code_id'] <= 3800]['ts_code']
-        codes = self.code_list['ts_code']
+        # codes = self.code_list['ts_code']
         # codes = ['002932.SZ']
         # codes = ['002901.SZ']
         # codes = ['002901.SZ']
         # codes = ['600267.SH', '002901.SZ', '300702.SZ', '603387.SH', '300685.SZ']
+        codes = ['002901.SZ', '002932.SZ', '603387.SH']
         if need_fields != '':
             fields = fields_map[api].copy()
             fields.remove('code_id')
