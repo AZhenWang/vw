6ea12fa32e98e3a8644feddb4294cae02addb1b5
王珍 <865820954@qq.com>
2020-08-19 17:22:36 +0800

    s

---
+++ app/batches/fetch_fx_daily.py
@@ -0,0 +1,19 @@
+from app.fetcher import ts
+
+
+def execute(start_date='', end_date=''):
+    worker = ts.Ts(start_date, end_date)
+
+    worker.set_trade_dates(is_open='')
+
+    worker.update_fx_obasic()
+    worker.set_fx_list()
+
+    if not worker.trade_dates.empty:
+
+        ts_apis = ['fx_daily']
+        for api in ts_apis:
+            worker.query_fx(api)
+
+
+
