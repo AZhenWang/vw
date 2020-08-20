679cc91293b44d236869bf2acc51258213069b50
王珍 <865820954@qq.com>
2020-08-20 12:00:03 +0800

    s

--- app/fetcher/ts.py
+++ app/fetcher/ts.py
@@ -447,6 +447,7 @@ class Ts(Interface):
     def query_fx(self, api):
         start_date = self.trade_dates.iloc[0]['cal_date']
         end_date = self.trade_dates.iloc[-1]['cal_date']
+        print('self.fx_list=', self.fx_list)
         for fx_obasic in self.fx_list:
             print('fx_obasic=', fx_obasic)
             fx_id = fx_obasic.fx_id
